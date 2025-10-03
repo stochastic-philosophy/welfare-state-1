08d07651b6ce9bfc1a37e2b8d396815ebc1e63b8
stochastic-philosophy <stochasticphilosophy@gmail.com>
2025-10-04 02:41:32 +0300

    Update utils.js

--- docs/utils.js
+++ docs/utils.js
@@ -1,1 +1,12 @@
-
+// Apufunktio slugien luontiin
+function slugify(text) {
+  return text.toString().toLowerCase()
+    .replace(/ä/g, 'a')
+    .replace(/ö/g, 'o')
+    .replace(/å/g, 'a')
+    .replace(/\s+/g, '-')
+    .replace(/[^a-z0-9-]/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+}
