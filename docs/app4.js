07f887becb5be1bba4f1c11aa3df73f326b8f9a8
stochastic-philosophy <stochasticphilosophy@gmail.com>
2025-10-04 02:23:25 +0300

    Update and rename app.js to app4.js

--- docs/app4.js
+++ docs/app4.js
@@ -1,4 +1,4 @@
-// app.js
+// app4.js
 // 1. Tuodaan tarvittavat luokat ja kirjastot omista tiedostoistaan
 import { Router } from './router.js';
 import { HomeController, SectionController, ChapterController } from './controllers.js';
