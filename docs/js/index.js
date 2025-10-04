18260abaadd71c5dea414482241422202fa45548
stochastic-philosophy <stochasticphilosophy@gmail.com>
2025-10-04 09:43:42 +0300

    Update index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,17 +1,14 @@
-import { initThemeSwitcher } from './theme-switcher.js';
-import { initCookieConsent } from './cookie-consent.js';
-import { initFooter } from './footer.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initCookieConsent();
-  initThemeSwitcher();
-  initFooter();
-  
-  const spaButton = document.createElement('button');
-  spaButton.id = 'spa-link';
-  spaButton.textContent = '📄 js-versio';
-  spaButton.addEventListener('click', () => {
-    window.location.href = 'index.html';
-  });
-  document.body.appendChild(spaButton);
+import { initThemeSwitcher } from './js/theme-switcher.js';
+import { initCookieConsent } from './js/cookie-consent.js';
+import { initFooter } from './js/footer.js';
+        
+initCookieConsent();
+const staticButton = document.createElement('button');
+staticButton.id = 'static-link';
+staticButton.textContent = '🤖 Botti-versio';
+staticButton.addEventListener('click', () => {
+  window.location.href = 'static.html';
 });
+document.body.appendChild(staticButton);
+initThemeSwitcher();
+initFooter();
