0bdbccfbfc2dd931a88a74182f2c923a23f9c3ea
stochastic-philosophy <stochasticphilosophy@gmail.com>
2025-10-04 03:09:48 +0300

    Update markdown-config.js

--- docs/markdown-config.js
+++ docs/markdown-config.js
@@ -1,89 +1,12 @@
 // markdown-config.js
-// YKSINKERTAINEN VERSIO - EI ULKOISIA RIIPPUVUUKSIA
+// Markdown-parserin konfiguraatio
 
-/**
- * Yksinkertainen markdown-parseri regex-pohjaisesti
- * Tukee: otsikot, listat, lihavointi, kursivointi, linkit, koodiblokkit
- */
-function parseMarkdown(markdown) {
-  let html = markdown;
-  
-  // Koodiblokkit (```...```)
-  html = html.replace(/```([a-z]*)\n([\s\S]*?)```/g, (match, lang, code) => {
-    return `<pre><code class="language-${lang || 'text'}">${escapeHtml(code.trim())}</code></pre>`;
-  });
-  
-  // Inline-koodi (`...`)
-  html = html.replace(/`([^`]+)`/g, '<code>$1</code>');
-  
-  // Otsikot
-  html = html.replace(/^#### (.+)$/gm, '<h4>$1</h4>');
-  html = html.replace(/^### (.+)$/gm, '<h3>$1</h3>');
-  html = html.replace(/^## (.+)$/gm, '<h2>$1</h2>');
-  html = html.replace(/^# (.+)$/gm, '<h1>$1</h1>');
-  
-  // Lihavointi ja kursivointi
-  html = html.replace(/\*\*\*(.+?)\*\*\*/g, '<strong><em>$1</em></strong>');
-  html = html.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>');
-  html = html.replace(/\*(.+?)\*/g, '<em>$1</em>');
-  html = html.replace(/___(.+?)___/g, '<strong><em>$1</em></strong>');
-  html = html.replace(/__(.+?)__/g, '<strong>$1</strong>');
-  html = html.replace(/_(.+?)_/g, '<em>$1</em>');
-  
-  // Linkit [teksti](url)
-  html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
-  
-  // Kuvat ![alt](url)
-  html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)/g, '<img src="$2" alt="$1">');
-  
-  // Vaakaviiva
-  html = html.replace(/^---$/gm, '<hr>');
-  html = html.replace(/^\*\*\*$/gm, '<hr>');
-  
-  // Lainaukset
-  html = html.replace(/^> (.+)$/gm, '<blockquote>$1</blockquote>');
-  
-  // Listat - numeroitu
-  html = html.replace(/^\d+\. (.+)$/gm, '<li>$1</li>');
-  html = html.replace(/(<li>.*<\/li>)/s, '<ol>$1</ol>');
-  
-  // Listat - järjestämätön
-  html = html.replace(/^[\*\-\+] (.+)$/gm, '<li>$1</li>');
-  html = html.replace(/(<li>.*<\/li>)/s, '<ul>$1</ul>');
-  
-  // Kappaleet (kaksoistila)
-  html = html.replace(/\n\n+/g, '</p><p>');
-  html = '<p>' + html + '</p>';
-  
-  // Siivoa tyhjät kappaleet
-  html = html.replace(/<p><\/p>/g, '');
-  html = html.replace(/<p>(<h[1-6]>)/g, '$1');
-  html = html.replace(/(<\/h[1-6]>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<hr>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<pre>)/g, '$1');
-  html = html.replace(/(<\/pre>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<ul>)/g, '$1');
-  html = html.replace(/(<\/ul>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<ol>)/g, '$1');
-  html = html.replace(/(<\/ol>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<blockquote>)/g, '$1');
-  html = html.replace(/(<\/blockquote>)<\/p>/g, '$1');
-  
-  return html;
-}
+import MarkdownIt from 'https://cdn.jsdelivr.net/npm/markdown-it@14/+esm'
 
-function escapeHtml(text) {
-  const map = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#039;'
-  };
-  return text.replace(/[&<>"']/g, m => map[m]);
-}
-
-// Viedään yhteensopiva rajapinta
-export const md = {
-  render: parseMarkdown
-};
+// Alustetaan ja viedään markdown-parseri
+export const md = new MarkdownIt({
+  html: true,          // Sallii HTML-tagit markdownissa
+  linkify: true,       // Muuttaa URL:t automaattisesti linkeiksi
+  typographer: true,   // Kauniit lainausmerkit ja viivat
+  breaks: false        // Ei muuta rivinvaihtoja <br>:iksi
+});
